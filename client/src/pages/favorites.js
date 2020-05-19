0824de15d423dff040a9691e0ec9f120993e4f8a
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-19 11:57:51 -0500

    Added message that there are no books in the favorites when it applies

--- client/src/pages/favorites.js
+++ client/src/pages/favorites.js
@@ -28,18 +28,22 @@ function Favorites() {
   return (
     <div className="container text-center">
       <h3>Favorites</h3>
-      {books.map(book => {
-        return (<Book
-          key = {book._id}
-          title={book.title}
-          image={book.image}
-          link={book.link}
-          description={book.description}
-          author={book.authors}
-          icon = {"fas fa-trash"}
-          action = {()=>{deleteBook(book._id)}}
-        />)
-      })}
+      {books.length > 0?
+        books.map(book => {
+          return (<Book
+            key = {book._id}
+            title={book.title}
+            image={book.image}
+            link={book.link}
+            description={book.description}
+            author={book.authors}
+            icon = {"fas fa-trash"}
+            action = {()=>{deleteBook(book._id)}}
+          />)
+        })
+        :
+        <div className="text-danger h4"> You have no books saved in your favorites! </div>
+      }
     </div>
   );
 }
